69068d82840141f3325153554492abd631fd7d94
user <user@users-MBP.Dlink>
2020-08-11 00:03:25 +0300

    firebase playgrounding

---
+++ build/precache-manifest.51dd651e7011ac2abe4ce1a6079713df.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215080803bfd87e2d6ff242f0da20914",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1b9acb7bcf54ac25be1a",
+    "url": "./static/css/main.c83299b8.chunk.css"
+  },
+  {
+    "revision": "626fb246eefc018af957",
+    "url": "./static/js/2.c5428e87.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "./static/js/2.c5428e87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b9acb7bcf54ac25be1a",
+    "url": "./static/js/main.dc87d072.chunk.js"
+  },
+  {
+    "revision": "29d90d6f614c3453bd7d",
+    "url": "./static/js/runtime-main.0bb869ba.js"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "./static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "ee914aafc06a68860cc1a829707ffab4",
+    "url": "./static/media/getFetch.ee914aaf.cjs"
+  }
+]);
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.scss';
 import ErrorBoundary from "./components/errorHandler/ErrorBoundary";
 import Layout from "./containers/layout/Layout";
@@ -9,6 +9,7 @@ import LoginPage from "./components/login/LoginPage";
 import { growlState } from "./stores/growlStore/growlStore";
 import { hideGrowl, showMessage } from "./stores/growlStore/growlEvents";
 import { IGrowl } from "./model/misc/IGrowl";
+import { db } from "./firebase";
 
 initializeIcons();
 growlState
@@ -19,8 +20,66 @@ growlState
     .on(hideGrowl, () => ({title: '', description: '', isVisible: false}));
 
 function App() {
+
+    const [users, setUsers] = useState([{
+        id: 'dsf',
+        carma: 0,
+        country: "rusland",
+        name: "ivan",
+        points: 0,
+        registration: new Date()
+    }]);
+
+
+    useEffect(() => {
+        return db.collection('users').onSnapshot((snapshot => {
+            const docs: any[] = [];
+            snapshot.forEach((doc) => {
+                // console.log(doc);
+                // console.log(doc.data());
+                docs.push({
+                    ...doc.data(),
+                    id: doc.id
+                });
+            });
+
+            setUsers(docs);
+            console.log(docs);
+        }));
+    }, []);
+
   return (
     <div className="App">
+        <input type="text"
+               style={{position: "absolute",
+               zIndex: 999999
+               }}
+               onKeyDown={(e) => {
+            if (e.key === "Enter") {
+                //@ts-ignore
+                let val = e.target.value;
+
+                console.log(val);
+
+                db  //rooms/8dPoVTMHfBifOg3hoEuM/messages
+                    .collection("rooms")
+                    .doc("8dPoVTMHfBifOg3hoEuM")
+                    .collection("messages")
+                    .add({
+                        text: val,
+                        createdAt: new Date()
+                    })
+            }
+        }}/>
+        {
+            users.map((item:any) => {
+                return <div style={{position: "relative", zIndex: 99999, background: "steelblue"}}>
+                    <span>{item.id}</span>
+                    <span>{item.carma}</span>
+                    <span>{item.points}</span>
+                </div>
+            })
+        }
         <ErrorBoundary>
             <Router history={history}>
                 <Layout />
