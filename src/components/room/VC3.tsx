ce05fb10e576ec084ef2f6f32d61fc3910b62fb6
user <user@users-MBP.Dlink>
2020-10-12 09:59:37 +0300

    new lib installed

--- src/components/room/VC3.tsx
+++ src/components/room/VC3.tsx
@@ -3,17 +3,46 @@ import {IUser} from "../../model/user/IUser";
 import {configuration} from "./roomConnectionConfig";
 import {db} from "../../firebase";
 
+import SimplePeer from "simple-peer";
+import Peer from "peerjs";
+
 interface IProps {
     roomId: string;
     users: any[];
     user: IUser;
 }
 
+let peer = new Peer();
+
 export const VC3: FC<IProps> = ({roomId, user, users}) => {
 
     let userVideo = useRef(null);
     let partnerVideo = useRef(null);
 
+
+    useEffect(() => {
+        peer.on('open', (peerID) => {
+            console.log('peer id', peerID);
+        });
+    }, []);
+
+
+    useEffect(() => {
+        if (users && users.length && users[0].id &&
+            user && user.uid) {
+
+            if (users[0].id == user.uid) {
+
+            }
+
+            if (users.length > 1 && users[1].id === user.uid) {
+
+            }
+
+
+        }
+
+    }, [users])
 
     return <>
         <textarea
@@ -22,9 +51,10 @@ export const VC3: FC<IProps> = ({roomId, user, users}) => {
         </textarea>
 
         <video playsInline muted
-               style={{width: '600px', height: '420px', transform: 'rotateY(180deg)'}}
+               style={{transform: 'rotateY(180deg)'}}
                ref={userVideo} autoPlay/>
         <video playsInline muted
+               style={{transform: 'rotateY(180deg)'}}
                ref={partnerVideo} autoPlay/>
     </>
 }
