4582861a654d1c92d6a7d5eccfe8147b6311ee2d
user <user@users-MBP.Dlink>
2020-10-19 23:16:05 +0300

    ver 3 mid

--- src/components/room/VC3.tsx
+++ src/components/room/VC3.tsx
@@ -1,7 +1,7 @@
 import React, {FC, useEffect, useRef} from "react";
 import {IUser} from "../../model/user/IUser";
 import {configuration} from "./roomConnectionConfig";
-import Peer from "peerjs";
+import Peer, {MediaConnection} from "peerjs";
 
 interface IProps {
     roomId: string;
@@ -14,8 +14,10 @@ export const VC3: FC<IProps> = ({roomId, user, users}) => {
 
     let userVideo = useRef(null);
     let partnerVideo = useRef(null);
+    let peercall: any;
 
-    let peer = new Peer(roomId + user.uid, {config: configuration});
+    let peer = new Peer(roomId.substr(0, 8) + user.uid.substr(0, 8),
+        {config: configuration});
 
     peer.on('open', (peerID) => {
         console.log('opening peer connection, peer id', peerID);
@@ -23,19 +25,18 @@ export const VC3: FC<IProps> = ({roomId, user, users}) => {
 
 
     peer.on('call', (call) => {
-        console.log('incomming call');
+        console.log('incoming call');
         peercall = call;
-        callanswer();
+        callanswer(call);
     })
 
-    let peercall: any;
 
-    const callanswer = () => {
+    const callanswer = (call: MediaConnection) => {
         navigator.mediaDevices.getUserMedia({audio: true, video: true})
             .then(function (mediaStream) {
                 // var video = document.getElementById('myVideo');
-                peercall.answer(mediaStream); // отвечаем на звонок и передаем свой медиапоток собеседнику
-                //peercall.on ('close', onCallClose); //можно обработать закрытие-обрыв звонка
+                call.answer(mediaStream); // отвечаем на звонок и передаем свой медиапоток собеседнику
+                //peercall.on('close', onCallClose); //можно обработать закрытие-обрыв звонка
                 if (userVideo.current) {
                     //@ts-ignore
                     userVideo.current.srcObject = mediaStream;
@@ -47,7 +48,7 @@ export const VC3: FC<IProps> = ({roomId, user, users}) => {
                         //@ts-ignore
                         partnerVideo.current.srcObject = peercall.remoteStream;
                     }
-                }, 1500);
+                }, 2000);
                 console.log('answering...');
             })
             .catch(function (err) {
@@ -59,18 +60,21 @@ export const VC3: FC<IProps> = ({roomId, user, users}) => {
         navigator.mediaDevices.getUserMedia({audio: true, video: true})
             .then(function (mediaStream) {
                 // var video = document.getElementById('myVideo');
-                peercall = peer.call(roomId + users[0].id, mediaStream); //звоним, указав peerId-партнера и передав свой mediaStream
+                let pcall = peer.call(roomId.substr(0, 8) + users[0].id.substr(0, 8),
+                    mediaStream); //звоним, указав peerId-партнера и передав свой mediaStream
 
-                console.log('call to', roomId + users[0].id);
+                peercall = pcall
 
-                peercall.on('stream', function (stream: any) { //нам ответили, получим стрим
+                console.log('calling to', roomId.substr(0, 8) + users[0].id.substr(0, 8));
+
+                pcall.on('stream', function (stream: any) { //нам ответили, получим стрим
                     setTimeout(function () {
                         if (partnerVideo.current) {
                             //@ts-ignore
-                            partnerVideo.current.srcObject = peercall.remoteStream;
+                            partnerVideo.current.srcObject = peercall.stream;
                         }
 
-                    }, 1500);
+                    }, 2000);
                 });
                 //  peercall.on('close', onCallClose);
                 if (userVideo.current) {
@@ -95,7 +99,8 @@ export const VC3: FC<IProps> = ({roomId, user, users}) => {
             }
 
             if (users.length > 1 && users[1].id === user.uid) {
-                callToNode();
+                setTimeout(callToNode, 2000);
+                // callToNode();
             }
 
 
