c13e430860185fdaaff592203f3940368cb4f8c5
Arnack <gri-go-riy@mail.ru>
2021-04-09 18:00:19 +0300

    minor code style changes

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,6 @@ import 'firebase/database';
 import 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB5P0AIbxKzHsjFrThNOwCsWnF0i4niF6s",
-    authDomain: "speaking-club-f98fd.firebaseapp.com",
-    databaseURL: "https://speaking-club-f98fd.firebaseio.com",
-    projectId: "speaking-club-f98fd",
-    storageBucket: "speaking-club-f98fd.appspot.com",
-    messagingSenderId: "169469776484",
-    appId: "1:169469776484:web:76fb149f8f6148b8170571",
-    measurementId: "G-DVQDJ8SCJR"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
